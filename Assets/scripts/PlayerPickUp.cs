ed82b94621aff5bdafc3dafa9fcb3143c41d306d
FlorianWiekhorst <florian.wiekhorst@haw-hamburg.de>
2018-02-01 13:22:10 +0100

    added pickUp(crystal) with sound

---
+++ Assets/scripts/PlayerPickUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickUp : MonoBehaviour {
+
+	Animator anim; // Reference to the player's animator component.
+	public AudioClip[] audioclip; // Creates an Array to store our GameSounds
+	[SerializeField] int energie = 0; //show me number of energie collected
+
+	void Awake() // Setting up references.
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	//Enter a Trigger
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.tag == "Energiecrystal") 
+		{
+			Destroy(other.gameObject);
+			energie++;
+			Playsound(0);
+		}
+	}
+
+	//  SOUND
+	void Playsound(int clip)
+	{
+		GetComponent<AudioSource>().clip = audioclip [clip];
+		GetComponent<AudioSource>().Play ();
+	}
+}
