1e2f52e30010cefc25797bedb76e0df8c95d1cd9
FlorianWiekhorst <florian.wiekhorst@haw-hamburg.de>
2018-02-27 22:19:44 +0100

    added skills
    + healskill partikel
    + Fireball.Animations

---
+++ Assets/scripts/FireballSkill.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballSkill : MonoBehaviour {
+
+	public float velX;
+	public float velY;
+	Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+		rb.velocity = new Vector2 (velX, velY);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Enemy") {
+			Destroy(gameObject);           
+		}
+	}
+}
